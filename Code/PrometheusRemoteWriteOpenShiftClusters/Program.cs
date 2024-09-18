b0bf0e28858abe72607101d9e17192ff39c48159
Rifat Erdem Sahin <info@pexabo.com>
2024-09-18 12:09:51 +0100

    update

--- Code/PrometheusRemoteWriteOpenShiftClusters/Program.cs
+++ Code/PrometheusRemoteWriteOpenShiftClusters/Program.cs
@@ -13,18 +13,18 @@ class Program
         // // Create a counter metric
         var counter = Metrics.CreateCounter("example_counter", "Counts things");
 
-        // // Simulate data being pushed
-        // for (int i = 0; i < 10; i++)
-        // {
-        //     counter.Inc();
-        //     await Task.Delay(1000); // Push every second
-        // }
+        // Simulate data being pushed
+        for (int i = 0; i < 10; i++)
+        {
+            counter.Inc();
+            await Task.Delay(1000); // Push every second
+        }
 
-        // // Expose metrics on a port for scraping
-        // // var server = new MetricServer(hostname: "localhost", port: 9091);
-        // // server.Start();
+        // Expose metrics on a port for scraping
+         var server = new MetricServer(hostname: "localhost", port: 9091);
+         server.Start();
 
-        // await Task.Delay(-1); // Keep the server running
+        await Task.Delay(-1); // Keep the server running
     }
 } 
 }
--- Code/PrometheusRemoteWriteOpenShiftClusters/obj/Debug/net8.0/PrometheusRemoteWriteOpenShiftClusters.AssemblyInfo.cs
+++ Code/PrometheusRemoteWriteOpenShiftClusters/obj/Debug/net8.0/PrometheusRemoteWriteOpenShiftClusters.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrometheusRemoteWriteOpenShiftClusters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf15b52e1ab099e8691f918e1af93ff34e08ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e71751a114d0cac1ef8046208928e4f691272f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrometheusRemoteWriteOpenShiftClusters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrometheusRemoteWriteOpenShiftClusters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
